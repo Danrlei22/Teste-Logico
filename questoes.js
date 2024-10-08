da2907709f38e2ec44773e5fddf5f8a162edad5c
danrlei <danrlei_vieira@hotmail.com>
2024-10-08 12:54:46 -0300

    Adicionado as perguntas completas no botão

--- questoes.js
+++ questoes.js
@@ -80,14 +80,7 @@ function completandoLogica(){
 /*-------------------------------------------------------------------------*/
 
 function lampada(){
-    /* Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em salas diferentes. 
-
-    Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. 
     
-    Seu objetivo é descobrir qual interruptor controla qual lâmpada. 
-    
-    Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?   */
-
     document.getElementById("resultado").innerText = `-Ligaria o interruptor 1 e deixaria ligado durante uns 15 minutos. 
 
     -Desligaria o interruptor 1 e ligaria o interruptor 2.
